5e6bb617b9944bceea72941eda3f430f084b8693
Abcight <abcightdev@gmail.com>
2023-04-07 17:29:26 +0200

    add raygui

--- project/project/main.cpp
+++ project/project/main.cpp
@@ -1,29 +1,70 @@
+#define RAYGUI_IMPLEMENTATION
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "raylib.h"
+#include "raygui.h";
+
 #include "structures/linked_list.h"
 #include <iostream>
 
-void draw_centered(const char* text, int height, int fontSize) {
-	int width = MeasureText(text, fontSize);
-	DrawText(text, (GetScreenWidth() - width) / 2, height, fontSize, DARKGRAY);
-}
+const char* STRUCTURES[] = {
+	"Linked list",
+	"Stack",
+	"Queue",
+	"Disjoint set",
+	"BST",
+	"AVL",
+	"BIT",
+	"PAT",
+	"Trie",
+	"k-d tree"
+};
 
 int main(void)
 {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+	const int screenWidth = 1280;
+	const int screenHeight = 720;
 
-	InitWindow(screenWidth, screenHeight, "Data Structure Visualiser");
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	SetTargetFPS(60);
+	InitWindow(screenWidth, screenHeight, "Data Structure Visualiser");
 
-	Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+	Rectangle leftPanel = Rectangle{ 2,2,0,0 };
+	Rectangle propPanel;
+	Rectangle viewPanel;
+
+	GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
+	GuiSetStyle(DEFAULT, TEXT_PADDING, 20);
 
 	while (!WindowShouldClose())
 	{
+		leftPanel.width = std::max(GetScreenWidth() / 5, 300);
+		leftPanel.height = GetScreenHeight() - 2;
+
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
-		draw_centered("Data Structure Visualiser", 20, 25);
-		draw_centered("Pawel Wilk", 45, 20);
+		GuiPanel(leftPanel, "Data structures");
+
+		Rectangle start = { leftPanel.x + 10, leftPanel.y + 30, leftPanel.width - 20, 30 };
+		for(auto structure : STRUCTURES) {
+			GuiButton(start, structure);
+			start.y += 32;
+		}
+
+		propPanel = leftPanel;
+		propPanel.y = start.y + 10;
+		propPanel.height -= propPanel.y - 2;
+		GuiPanel(propPanel, "Inspector");
+
+		viewPanel = leftPanel;
+		viewPanel.x += viewPanel.width + 2;
+		viewPanel.width = GetScreenWidth() - viewPanel.x - 2;
+		GuiPanel(viewPanel, "Structure viewer");
+
+		GuiDrawText("No structure works yet qwq...\n", viewPanel, TEXT_ALIGN_CENTER, DARKGRAY);
+		GuiDrawText("\nCome back later...", viewPanel, TEXT_ALIGN_CENTER, GRAY);
 
 		EndDrawing();
 	}
