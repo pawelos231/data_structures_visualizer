fe2ca4c5c8c3280d7ab6a71c404b8e41027c44bb
pawelLinek <pawellinek3D@gmail.com>
2023-06-04 18:13:29 +0200

    start hashmap Impl

--- project/project/structures/hash_map.h
+++ project/project/structures/hash_map.h
@@ -1,8 +1,30 @@
 #pragma once
 #include<iostream>
+#include <vector>
+#include <list>
+#include <stdexcept>
 
+
+//seperate chaining, open addresing  (think about using one of those)
+
+template<typename KeyType, typename ValueType>
 class HashMap {
-	HashMap() {
 
-	}
+private:
+	struct KeyValuePair {
+		KeyType key;
+		ValueType value;
+		KeyValuePair(const KeyType& k, const ValueType& v): key(k), value(v)
+	};
+
+	typedef std::list<KeyValuePair> Bucket;
+
+	std::vector<Bucket> buckets;
+	size_t size;
+	size_t capacity;
+	static const size_t DEFAULT_CAPACITY = 16;
+	
+
+public:
+	
 };
