8f1db970ad88dfcf11742520c607a5ebf6d01d84
pawelLinek <pawellinek3D@gmail.com>
2023-06-04 19:28:53 +0200

    added remove insert and get function to hashmap

--- project/project/structures/hash_map.h
+++ project/project/structures/hash_map.h
@@ -5,7 +5,8 @@
 #include <stdexcept>
 
 
-//seperate chaining, open addresing  (think about using one of those)
+//i decided to use seperate chaining, bucket refers to datastructure i am storing it in in this case its linked list
+//for hashing function i choose standard library std::hash instead of relieing on my own implementetion
 
 template<typename KeyType, typename ValueType>
 class HashMap {
@@ -14,7 +15,6 @@ private:
 	struct KeyValuePair {
 		KeyType key;
 		ValueType value;
-		KeyValuePair(const KeyType& k, const ValueType& v): key(k), value(v)
 	};
 
 	typedef std::list<KeyValuePair> Bucket;
@@ -23,8 +23,64 @@ private:
 	size_t size;
 	size_t capacity;
 	static const size_t DEFAULT_CAPACITY = 16;
-	
+
+	size_t getBucketIndex(const KeyType& key) const {
+		return std::hash<KeyType>{}(key) % capacity;
+	};
 
 public:
-	
+
+	HashMap(size_t initialCapacity = DEFAULT_CPACITY) : capacity(initialCapacity), size(0) {
+		this->buckets.resize(capacity);
+	}
+
+	void insert(KeyType& const key, ValueType& const value) {
+
+		size_t bucketIndex = this->getBucketIndex(key);
+
+		for (const auto& kvp : buckets[getBucketIndex]) {
+			if (kvp.key == key) {
+				throw std::invalid_argument("Duplicate key");
+			}
+		}
+
+		KeyValuePair kv{ key, value };
+		buckets[bucketIndex].push_back(kv);
+
+		if (size > capacity) {
+			size_t newCapacity = capacity * 2;
+			this->rehash(newCapacity);
+		}
+	}
+
+	ValueType get(KeyType& const key) const {
+
+		size_t bucketIndex = this->getBucketIndex(key);
+
+		for (const auto& kvp : buckets[getBucketIndex]) {
+			if (kvp.key == key) {
+				return kvp.value
+			}
+		}
+		throw std::out_of_range("Key not found");
+
+	}
+
+	void remove(KeyType& const key) {
+		size_t bucketIndex = this->getBucketIndex(key);
+
+		for (auto it = buckets[bucketIndex].begin(); it != buckets[bucketIndex].end(); ++it) {
+			if (it->key == key) {
+				buckets[bucketIndex].erase(kvp);
+				size--;
+				return;
+			}
+		}
+		throw std::out_of_range("Key not found");
+
+
+	}
+
 };
+
+
