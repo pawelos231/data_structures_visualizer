b9ab0d3c5012988339ed73c0056b8a7cb5f89b5f
pawelLinek <pawellinek3D@gmail.com>
2023-07-01 03:15:54 +0200

    stack implementation
    stack implementation

--- project/project/structures/stack.h
+++ project/project/structures/stack.h
@@ -1,1 +1,38 @@
 #pragma once
+#include <iostream>
+#include <vector>
+
+template <typename T>
+class Stack {
+private:
+	std::vector<T> elements;
+
+public:
+	void push(const T& element) {
+		elements.push_back(element);
+	}
+
+	T pop() {
+		if (empty()) {
+			throw std::out_of_range("Stack is empty");
+		}
+		T element = elements.back();
+		elements.pop_back();
+		return element;
+	}
+
+	T top() const {
+		if (empty()) {
+			throw std::out_of_range("Stack is empty");
+		}
+		return elements.back();
+	}
+
+	size_t size() const {
+		return elements.size();
+	}
+
+	bool empty() const {
+		return elements.empty();
+	}
+};
