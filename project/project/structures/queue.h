896c0c52cc365de98b31af65da1ca22f256dd7e8
pawelLinek <pawellinek3D@gmail.com>
2023-07-01 21:55:08 +0200

    array based queue impl
    array based queue impl, think about adding linked list based queue for constant retrieval and deletion times

--- project/project/structures/queue.h
+++ project/project/structures/queue.h
@@ -1,1 +1,37 @@
 #pragma once
+#include <vector>
+template <typename T>
+class QueueArrayBased {
+private:
+	std::vector<T> queue;
+public:
+
+	void enqueue(T item) {
+		this->queue.push_back(item);
+	}
+
+	const T dequeue() {
+		if (this->empty()) {
+			throw std::out_of_range("Queue is empty");
+		}
+		T frontElement = queue.front();
+		this->queue.erase(this->queue.begin());
+		return frontElement;
+	}
+
+
+	const T front() const {
+		if (empty()) {
+			throw std::out_of_range("Queue is empty");
+		}
+		return this->queue.front();
+	}
+
+	const size_t size() const {
+		return this->queue.size();
+	}
+
+	const bool empty() const {
+		return this->queue.empty();
+	}
+};
