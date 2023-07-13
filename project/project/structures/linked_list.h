37d99b31b92827db1d89240ecff5d22e5526d9de
pawelLinek <pawellinek3D@gmail.com>
2023-07-13 21:35:04 +0200

    doubly linked list impl

--- project/project/structures/linked_list.h
+++ project/project/structures/linked_list.h
@@ -148,21 +148,119 @@ public:
 
 template <typename T>
 class DoublyLinkedList {
-
 private:
 	class Node {
 	public:
 		T data;
-		Node* next;
 		Node* prev;
+		Node* next;
 
-		Node() : next(nullptr), prev(nullptr) {}
-		explicit Node(T data) : data(data), next(nullptr) prev(nullptr) {}
-
+		Node() : prev(nullptr), next(nullptr) {}
+		explicit Node(T data) : data(data), prev(nullptr), next(nullptr) {}
 	};
+
 	Node* head;
+	Node* tail;
 	int len;
 
-	DoublyLinkedList() : head(nullptr), len(0) {}
+public:
+	DoublyLinkedList() : head(nullptr), tail(nullptr), len(0) {}
 
+	int length() const {
+		return len;
+	}
+
+	void push(T data) {
+		Node* newNode = new Node(data);
+
+		if (head == nullptr) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			newNode->prev = tail;
+			tail->next = newNode;
+			tail = newNode;
+		}
+
+		len++;
+	}
+
+	void insert(int index, T data) {
+		if (index < 0 || index > len) {
+			throw std::out_of_range("Invalid index");
+		}
+
+		if (index == len) {
+			push(data);
+			return;
+		}
+
+		Node* newNode = new Node(data);
+
+		if (index == 0) {
+			newNode->next = head;
+			head->prev = newNode;
+			head = newNode;
+		}
+		else {
+			Node* temp = head;
+			for (int i = 0; i < index - 1; i++) {
+				temp = temp->next;
+			}
+
+			newNode->prev = temp;
+			newNode->next = temp->next;
+			temp->next->prev = newNode;
+			temp->next = newNode;
+		}
+
+		len++;
+	}
+
+	void remove(int index) {
+		if (index < 0 || index >= len) {
+			throw std::out_of_range("Invalid index");
+		}
+
+		Node* temp = head;
+
+		if (index == 0) {
+			head = head->next;
+			if (head != nullptr) {
+				head->prev = nullptr;
+			}
+			else {
+				tail = nullptr;
+			}
+		}
+		else if (index == len - 1) {
+			temp = tail;
+			tail = tail->prev;
+			tail->next = nullptr;
+		}
+		else {
+			for (int i = 0; i < index; i++) {
+				temp = temp->next;
+			}
+			temp->prev->next = temp->next;
+			temp->next->prev = temp->prev;
+		}
+
+		delete temp;
+		len--;
+	}
+
+	void print() {
+		std::cout << "[";
+		Node* temp = head;
+		while (temp != nullptr) {
+			std::cout << temp->data;
+			if (temp->next != nullptr) {
+				std::cout << ", ";
+			}
+			temp = temp->next;
+		}
+		std::cout << "]" << std::endl;
+	}
 };
