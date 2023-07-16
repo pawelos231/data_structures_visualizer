a8ba2955bc5f2d050c74c96f0e7e55f0ffe336aa
pawelLinek <pawellinek3D@gmail.com>
2023-07-16 19:40:49 +0200

    PQ impl

--- project/project/structures/priority_queue.h
+++ project/project/structures/priority_queue.h
@@ -1,5 +1,7 @@
 #pragma once
-//priority queues are strongly realated to heaps, and the most common implementatios include them
+#include "./max_heap.h"
+
+//priority queues are strongly realated to heaps, and the most common implementatios includes max heap
 
 /*
 Use cases:
@@ -9,3 +11,46 @@ Use cases:
  - Task Scheduling and Job Prioritization
  - Job Scheduling in Cloud Computing
 */
+
+
+template<typename T>
+class PriorityQueue {
+private:
+	MaxHeap<T> maxHeap;
+
+public:
+	PriorityQueue() : maxHeap(MaxHeap<T>()) {}
+	PriorityQueue(int cap) : maxHeap(MaxHeap<T>(cap)) {}
+
+	void insert(const T& data, int priority) {
+		maxHeap.insert(data, priority);
+	}
+
+	void insert(const T& data) {
+		maxHeap.insert(data);
+	}
+
+	void clear() {
+		maxHeap.clear();
+	}
+
+	void remove() {
+		maxHeap.remove();
+	}
+
+	bool isEmpty() const {
+		return maxHeap.isEmpty();
+	}
+
+	std::size_t size() const {
+		return maxHeap.size();
+	}
+
+	const T& peek() const {
+		return maxHeap.peek();
+	}
+
+	void logHeap() const {
+		maxHeap.logHeap();
+	}
+};
